8caffe3d480f2f044ed5871cf8d4b9cf0faeb97d
leoemaxie <leoemaxie@gmail.com>
2023-10-16 22:02:22 +0100

    Model tests

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Unittests for the Review Model"""
+import unittest
+from test_models.base_test import BaseTestCase
+from models.review import Review
+
+
+class ReviewTestCase(BaseTestCase, unittest.TestCase):
+    """Tests the Review Model"""
+
+    def setUp(self):
+        """Sets up the model"""
+        self.model = Review
+        self.Model1 = Review()
+        self.Model2 = Review(**self.Model1.to_dict())
+        self.docs = __import__(
+            "models.user",
+            fromlist="user"
+        ).__doc__
+
+
+if __name__ == "__main__":
+    unittest.main()
